0c5bd406ecaf7ac1eaa0627c6d297b900bab3d0f
Dennis Lord <lorddennis1006@gmail.com>
2018-04-13 13:21:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  index.between?(0, 8) && !(index_taken?(board, number_entered))
+  index.between?(0, 8) && !(index_taken?(board, index))
 end
 
 def position_taken?(board, index)
