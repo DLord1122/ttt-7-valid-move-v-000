27cd278648a714fefd231c04418194575ca785cb
Dennis Lord <lorddennis1006@gmail.com>
2018-04-13 13:55:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,8 @@
 def valid_move?(board, index)
-  taken= nil
   if (board[index] == " ") && (board[index] == "")
     taken = false
   else
-    taken = true
+     true
 end
 end
 def position_taken?(board, index)
