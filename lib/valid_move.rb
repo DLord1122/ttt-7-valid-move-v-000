1243fea522fcff723ce9796f9b4f59ca5e0713fb
Dennis Lord <lorddennis1006@gmail.com>
2018-04-13 13:19:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,13 @@ def valid_move?(number_entered)
   number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
 
-def position_taken?(board, index)
- !(board[index] == " " || board[index] == "" || board[index] == nil)
+ddef position_taken?(board, index)
+
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+   taken = false
+  else
+  taken = true
+  end
+  taken 
 end
